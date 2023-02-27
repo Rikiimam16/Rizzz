d14dfc5ec6d40479c9963b61315300c3017ea3df
Safari112 <126477280+Safari112@users.noreply.github.com>
2023-02-27 21:44:50 +0700

    Sjs
    Hddudj

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.language = english  //change indonesia to english if you don't understand
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
+global.botname = "RiksBotz-Mdོ" //namabot kalian
 global.ownername= "ᴹᴿ᭄ RASHMIKA ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
